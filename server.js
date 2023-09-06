91a4153886ca648d9fffb0717df1beafc6195c33
satyam <satyambsingh93@gmail.com>
2023-09-06 14:53:30 +0530

    get user details

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ app.get('/api/get',(req,res) => {
     res.send({message:"hello back to nodejs"})
 })
 
+app.get('/api/get_user_details',(req,res) => {
+    res.send({
+        user:{
+            name:"satyam",
+            age:22,
+            contact:12254
+        }
+    })
+});
+
 
 
 app.listen(8000,() => {
