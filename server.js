53d78d5865352f959cc27ec005c2f567242ae992
satyam <satyambsingh93@gmail.com>
2023-09-06 15:01:36 +0530

    added env

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-require("dotenv").config
+require("dotenv").config()
 
 app.get('/api/get',(req,res) => {
     res.send({message:"hello back to nodejs"})
